4e435ea4b177f683ed7516b03b8d50c0116206fb
Sandy Tam <111902534+satam2@users.noreply.github.com>
2025-03-07 13:46:50 -0800

    Create View.java

---
+++ mvc/View.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class View extends JPanel implements Subscriber {
+    protected Model model;
+
+    public View(Model model) {
+        this.model = model;
+        model.subscribe(this);
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model newModel) {
+        model.unsubscribe(this);
+        model = newModel;
+        newModel.subscribe(this);
+        // Pull data from newModel
+        update();
+    }
+
+    @Override
+    public void update() {
+        repaint();
+    }
+}
