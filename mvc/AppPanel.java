ec7471844a3042b1b7fa3e18557fe85368cb36ee
Sandy Tam <111902534+satam2@users.noreply.github.com>
2025-03-07 13:48:21 -0800

    mvc framework

---
+++ mvc/AppPanel.java
@@ -0,0 +1,125 @@
+package mvc;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class AppPanel extends JPanel implements Subscriber, ActionListener {
+    protected AppFactory factory;
+    protected Model model;
+
+    protected JPanel controlPanel;
+    protected View view;
+    protected SafeJFrame frame;
+
+    public AppPanel(AppFactory factory) {
+        this.factory = factory;
+        this.model = factory.makeModel();
+        this.model.subscribe(this);
+
+        controlPanel = new JPanel();
+        controlPanel.setBackground(Color.PINK);
+        controlPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        view = factory.makeView(model);
+
+        setLayout((new GridLayout(1, 2)));
+        add(controlPanel);
+        add(view);
+
+        frame = new SafeJFrame();
+
+        frame.setJMenuBar(makeMenuBar());
+        frame.add(this);
+        frame.setTitle(factory.getTitle());
+        frame.setSize(650, 400);
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model newModel) {
+        model.unsubscribe(this);
+        model = newModel;
+        model.subscribe(this);
+        view.setModel(model);
+    }
+
+    public void display() {
+        frame.setVisible(true);
+    }
+
+    private JMenuItem createMenuItem(String name) {
+        var res = new JMenuItem(name);
+        res.addActionListener(this);
+        return res;
+    }
+
+    @Override
+    public void update() {
+        // No-op
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String name = e.getActionCommand();
+        boolean cmdResolved = true;
+        switch (name) {
+            case "New" -> setModel(factory.makeModel());
+            // If no filename is associated, a save-as prompt is opened automatically
+            case "Save" -> Utilities.save(model, false);
+            case "Save As" -> Utilities.save(model, true);
+            case "Open" -> setModel(Utilities.open(model));
+            case "Quit" -> System.exit(0);
+            default -> cmdResolved = false;
+        }
+        if (cmdResolved)
+            return;
+
+        Command cmd = factory.makeEditCommand(model, name, e.getSource());
+        if (cmd != null) {
+            try {
+                cmd.execute();
+            } catch (Exception ex) {
+                Utilities.error(ex);
+            }
+        }
+    }
+
+    protected JMenuBar makeMenuBar() {
+        var mb = new JMenuBar();
+
+        var files = new JMenu("Files");
+        files.add(createMenuItem("New"));
+        files.add(createMenuItem("Save"));
+        files.add(createMenuItem("Save As"));
+        files.add(createMenuItem("Open"));
+        files.add(createMenuItem("Quit"));
+        mb.add(files);
+
+        var edit = new JMenu("Edit");
+        for (String name : factory.getEditCommands()) {
+            var item = new JMenuItem(name);
+            item.setActionCommand(name); // technically not necessary, left here for clarity: we DO want the command name to be used as the key
+            item.addActionListener(this);
+            edit.add(item);
+        }
+        mb.add(edit);
+
+        var help = new JMenu("Help");
+        {
+            var helpItem = new JMenuItem("Help...");
+            helpItem.addActionListener(e -> Utilities.inform(factory.getHelp()));
+            help.add(helpItem);
+        }
+        {
+            var aboutItem = new JMenuItem("About...");
+            aboutItem.addActionListener(e -> Utilities.inform(factory.about()));
+            help.add(aboutItem);
+        }
+        mb.add(help);
+
+        return mb;
+    }
+}
---
+++ mvc/Model.java
@@ -0,0 +1,18 @@
+package mvc;
+
+import java.io.*;
+
+public class Model extends Publisher implements Serializable {
+    protected boolean unsavedChanges = false;
+    protected String fileName = null;
+
+    public void changed() {
+        unsavedChanges = true;
+        notifySubscribers();
+    }
+
+    public boolean getUnsavedChanges() { return unsavedChanges; }
+    public void setUnsavedChanges(boolean unsavedChanges) { this.unsavedChanges = unsavedChanges; }
+    public String getFileName() { return fileName; }
+    public void setFileName(String fileName) { this.fileName = fileName; }
+}
