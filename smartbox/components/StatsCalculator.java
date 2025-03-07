4fc5e84657f8d6a12e4b4a1df593b73ba8259b9d
Sandy Tam <111902534+satam2@users.noreply.github.com>
2025-03-07 13:50:54 -0800

    Create StatsCalculator.java

---
+++ smartbox/components/StatsCalculator.java
@@ -0,0 +1,32 @@
+package smartbox.components;
+import java.util.*;
+import smartbox.*;
+
+public class StatsCalculator extends Component implements App {
+
+    public ICalculator arithmeticCalculator;
+
+    public StatsCalculator() {
+        super();
+    }
+
+    public Double mean(List<Double> data) throws Exception {
+        Double sum = 0.0;
+        for(Double val: data) {
+            sum = arithmeticCalculator.add(sum, val);
+        }
+        Double avg = arithmeticCalculator.div(sum, (double)data.size());
+        return avg;
+    }
+
+    public void main() throws Exception {
+
+        List<Double> scores = new LinkedList<Double>();
+        for(int i = 0; i < 100; i++) {
+            scores.add((double)i);
+        }
+        Double avg = mean(scores);
+        mvc.Utilities.inform("Average = " + avg);
+    }
+
+}
